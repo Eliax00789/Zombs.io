f41f6eb1e2f3b20956abf27611c9423c2d7bc95a
Niffler77 <niffler77@gmx.net>
2022-08-10 16:56:43 +0200

    Tower Building Begining Resources Command Feedback

--- src/main/java/me/eliax00789/zombsio/buildings/towers/Tower.java
+++ src/main/java/me/eliax00789/zombsio/buildings/towers/Tower.java
@@ -1,4 +1,13 @@
 package me.eliax00789.zombsio.buildings.towers;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+
 public class Tower {
+
+    public void build(Location location, Material[][][] material) {
+
+        location.getBlock().setType(material[0][0][0]);
+    }
 }
