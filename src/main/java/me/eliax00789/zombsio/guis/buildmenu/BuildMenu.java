89acc08bf521703a0f7ef7d0326215c92fa7c03e
BuildTools <unconfigured@null.spigotmc.org>
2022-08-10 14:17:47 +0200

    Bugfixes

--- src/main/java/me/eliax00789/zombsio/guis/buildmenu/BuildMenu.java
+++ src/main/java/me/eliax00789/zombsio/guis/buildmenu/BuildMenu.java
@@ -1,10 +1,14 @@
 package me.eliax00789.zombsio.guis.buildmenu;
 
 import me.eliax00789.zombsio.Zombsio;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.logging.Level;
 
 public class BuildMenu implements Listener {
 
@@ -12,10 +16,11 @@ public class BuildMenu implements Listener {
         Zombsio.plugin.getServer().getPluginManager().registerEvents(this,Zombsio.plugin);
     }
     @EventHandler
-    public void onCraftingTablePlace(BlockPlaceEvent event) {
-        if (event.getBlockPlaced().getType().equals(Material.CRAFTING_TABLE)) {
-            event.getPlayer().openInventory(new BuildMenuInventory().getInventory());
-            event.setCancelled(true);
+    public void onCraftingTablePlace(BlockPlaceEvent e) {
+        if (e.getBlockPlaced().getType().equals(Material.CRAFTING_TABLE)) {
+            Bukkit.getLogger().log(Level.ALL,"Someone placed crafting table");
+            e.getPlayer().openInventory(new BuildMenuInventory().getInventory());
+            e.setCancelled(true);
         }
     }
 }
