17d19d76c6c0f69c0fa0756b2cdda3e709b6ef1c
Niffler77 <niffler77@gmx.net>
2022-08-10 17:27:03 +0200

    Started Workin on mk2

--- src/main/java/me/eliax00789/zombsio/buildings/towers/CannonTower.java
+++ src/main/java/me/eliax00789/zombsio/buildings/towers/CannonTower.java
@@ -27,4 +27,28 @@ public class CannonTower {
                     {Material.AIR, Material.AIR, Material.AIR} // y = 4
             }
     };
+
+    public static Material[][][] mk2 = new Material[][][]{
+            { // x = 0
+                    {Material.OAK_LOG, Material.AIR, Material.OAK_LOG}, // y = 0
+                    {Material.OAK_PLANKS, Material.OAK_PLANKS, Material.OAK_PLANKS}, // y =
+                    {Material.AIR, Material.AIR, Material.AIR},  // y = 2
+                    {Material.IRON_TRAPDOOR, Material.IRON_TRAPDOOR, Material.IRON_TRAPDOOR}, // y = 3
+                    {Material.AIR, Material.AIR, Material.AIR} // y = 4
+            },
+            { // x = 1
+                    {Material.AIR, Material.AIR, Material.AIR}, // y = 0
+                    {Material.OAK_PLANKS, Material.DIRT, Material.OAK_PLANKS}, // y = 1
+                    {Material.OAK_SLAB, Material.OAK_SLAB, Material.OAK_SLAB},  // y = 2
+                    {Material.OAK_LOG, Material.OAK_LOG, Material.OAK_LOG}, // y = 3
+                    {Material.OAK_SLAB, Material.OAK_SLAB, Material.OAK_SLAB} // y = 4
+            },
+            { // x = 2
+                    {Material.OAK_LOG, Material.AIR, Material.OAK_LOG}, // y = 0
+                    {Material.OAK_PLANKS, Material.OAK_PLANKS, Material.OAK_PLANKS}, // y = 1
+                    {Material.AIR, Material.AIR, Material.AIR},  // y = 2
+                    {Material.IRON_TRAPDOOR, Material.IRON_TRAPDOOR, Material.IRON_TRAPDOOR}, // y = 3
+                    {Material.AIR, Material.AIR, Material.AIR} // y = 4
+            }
+    };
 }
