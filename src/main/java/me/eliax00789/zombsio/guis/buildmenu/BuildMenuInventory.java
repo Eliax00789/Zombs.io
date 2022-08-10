033599aa763c8c34a39ee6fb1ed01fb436c9a4be
BuildTools <unconfigured@null.spigotmc.org>
2022-08-10 21:54:19 +0200

    BuildMenuInventory fix #INFINITE

--- src/main/java/me/eliax00789/zombsio/guis/buildmenu/BuildMenuInventory.java
+++ src/main/java/me/eliax00789/zombsio/guis/buildmenu/BuildMenuInventory.java
@@ -23,90 +23,70 @@ class BuildMenuInventory {
                     @Override
                     public void run() {
 
-                        for (HumanEntity i: inventory.getViewers()) {
-                            i.closeInventory();
-                        }
+                        inventory.getViewers().get(0).closeInventory();
                     }
                 })
                 .setItem(11, new ItemCreator(Material.GOLD_NUGGET).setName("Gold Mine").getItem(), new BukkitRunnable() {
                     @Override
                     public void run() {
 
-                        for (HumanEntity i: inventory.getViewers()) {
-                            i.closeInventory();
-                        }
+                        inventory.getViewers().get(0).closeInventory();
                     }
                 })
                 .setItem(13, new ItemCreator(Material.IRON_BARS).setName("Wall").getItem(), new BukkitRunnable() {
                     @Override
                     public void run() {
                         new Wall(location);
-                        for (HumanEntity i: inventory.getViewers()) {
-                            i.closeInventory();
-                        }
+                        inventory.getViewers().get(0).closeInventory();
                     }
                 })
                 .setItem(14, new ItemCreator(Material.IRON_DOOR).setName("Door").getItem(), new BukkitRunnable() {
                     @Override
                     public void run() {
                         new Door(location);
-                        for (HumanEntity i: inventory.getViewers()) {
-                            i.closeInventory();
-                        }
+                        inventory.getViewers().get(0).closeInventory();
                     }
                 })
                 .setItem(16, new ItemCreator(Material.HONEYCOMB).setName("Slow Trap").getItem(), new BukkitRunnable() {
                     @Override
                     public void run() {
 
-                        for (HumanEntity i: inventory.getViewers()) {
-                            i.closeInventory();
-                        }
+                        inventory.getViewers().get(0).closeInventory();
                     }
                 })
                 .setItem(20, new ItemCreator(Material.ARROW).setName("Arrow Tower").getItem(), new BukkitRunnable() {
                     @Override
                     public void run() {
                         new ArrowTower(location);
-                        for (HumanEntity i: inventory.getViewers()) {
-                            i.closeInventory();
-                        }
+                        inventory.getViewers().get(0).closeInventory();
                     }
                 })
                 .setItem(21, new ItemCreator(Material.FIRE_CHARGE).setName("Cannon Tower").getItem(), new BukkitRunnable() {
                     @Override
                     public void run() {
                         new CannonTower(location);
-                        for (HumanEntity i: inventory.getViewers()) {
-                            i.closeInventory();
-                        }
+                        inventory.getViewers().get(0).closeInventory();
                     }
                 })
                 .setItem(22, new ItemCreator(Material.TNT).setName("Bomb Tower").getItem(), new BukkitRunnable() {
                     @Override
                     public void run() {
                         new BombTower(location);
-                        for (HumanEntity i: inventory.getViewers()) {
-                            i.closeInventory();
-                        }
+                        inventory.getViewers().get(0).closeInventory();
                     }
                 })
                 .setItem(23, new ItemCreator(Material.ENCHANTED_BOOK).setName("Mage Tower").addEnchant(Enchantment.MULTISHOT,1,false).getItem(), new BukkitRunnable() {
                     @Override
                     public void run() {
                         new MageTower(location);
-                        for (HumanEntity i: inventory.getViewers()) {
-                            i.closeInventory();
-                        }
+                        inventory.getViewers().get(0).closeInventory();
                     }
                 })
                 .setItem(24,new ItemCreator(Material.STONE_SWORD).setName("Melee Tower").getItem(), new BukkitRunnable() {
                     @Override
                     public void run() {
                         new MeleeTower(location);
-                        for (HumanEntity i: inventory.getViewers()) {
-                            i.closeInventory();
-                        }
+                        inventory.getViewers().get(0).closeInventory();
                     }
                 })
                 .getInventory();
