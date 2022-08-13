a85e810e00e4441242f1653e08943c98146a194f
Niffler77 <niffler77@gmx.net>
2022-08-13 19:40:54 +0200

    SlowTrap extendes Building now

--- src/main/java/me/eliax00789/zombsio/buildings/other/SlowTrap.java
+++ src/main/java/me/eliax00789/zombsio/buildings/other/SlowTrap.java
@@ -1,12 +1,25 @@
 package me.eliax00789.zombsio.buildings.other;
 
+import me.eliax00789.zombsio.Zombsio;
+import me.eliax00789.zombsio.buildings.Building;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
-public class SlowTrap {
+public class SlowTrap extends Building {
 
     //TODO: NIFFLER SLOW TRAP INTO MK THINGS
 
     public SlowTrap(Player player, Location location) {
+        super(player,"Wall",1,8,location,
+                null,null,null,
+                new Material[][][][]{null,null,null,null,null,null,null,null},
+                Zombsio.plugin.getConfig().getIntegerList("Buildings.Wall.Health"),
+                null,
+                null,
+                Zombsio.plugin.getConfig().getIntegerList("Buildings.Wall.Wood"),
+                Zombsio.plugin.getConfig().getIntegerList("Buildings.Wall.Stone"),
+                Zombsio.plugin.getConfig().getIntegerList("Buildings.Wall.Gold"));
     }
+
 }
