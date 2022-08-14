b1b0b42f28c1afa1ba6c1e1c075975a363dddbc0
BuildTools <unconfigured@null.spigotmc.org>
2022-08-14 18:53:22 +0200

    LOL

--- src/main/java/me/eliax00789/zombsio/entity/PlayerStats.java
+++ src/main/java/me/eliax00789/zombsio/entity/PlayerStats.java
@@ -172,7 +172,8 @@ public class PlayerStats implements Listener {
     @EventHandler
     public void onPlayerDamage(EntityDamageEvent e) {
         if (e.getEntity() instanceof Player) {
-            ((Player) e.getEntity()).setHealth(((Player) e.getEntity()).getHealth() + e.getDamage());
+            e.setCancelled(true);
+            e.getEntity().setVelocity(e.getEntity().getVelocity().multiply(100));
             Player player = (Player) e.getEntity();
             if (stats.get(player)[7] < 0) {
                 Double damage = e.getDamage() * 10;
